2b1d221eeeab855f040da77096169fd8b58b2092
Marc Domingo <mdsunday01@gmail.com>
2019-11-15 10:51:35 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# numbers.sh
+# Marc Domingo
+
+echo "Enter a positive integer:"
+read integer
+n=1
+
+while [ "$n" -le "$integer" ]
+do
+	if [ $((n%2)) -eq 0 ]
+	then
+		echo "$n even"
+		n=$[n+1]
+	else
+		echo "$n odd"
+		n=$[n+1]
+	fi
+done
